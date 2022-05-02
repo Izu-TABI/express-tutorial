031912d6f364383006bc3498d353a1b4da555059
Izu-tabi-14 <masakisama14@gmail.com>
2022-05-02 15:13:21 +0900

    master

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// router.use(mylogger);
+
+router.get('/', mylogger,  (req, res) => {
   res.send('ユーザーです');
 });
 
@@ -13,5 +15,10 @@ router.get("/:id", (req, res) => {
     res.send(`${req.params.id}のユーザー情報を取得しました。`)
 });
 
+//ミドルウェア
+function mylogger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const userRouter = require('./routes/user.js');
 
 const PORT = 3000;
 
-app.use(express.static('public'));
+// app.use(mylogger);
+// app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
     // console.log('hello express');
     // res.send('こんにちは');
-
+    res.render('index', {text:'nodejsとexpress'})
 });
 
 //ルーティング
 app.use('/user', userRouter);
+
 
 app.listen(PORT, () => console.log('サーバーが起動しました。'));
